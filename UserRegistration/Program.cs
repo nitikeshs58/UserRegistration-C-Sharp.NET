51c1e484b52f604271aa860834a99a4bf1ae49ee
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-02 12:33:58 +0530

    Added: Checked name is valid or not.

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
     class Program
     {
+        static string VALIDNAMEPATTERN = "^[A-Z][a-z]{2,}$";
+        Regex regName = new Regex(VALIDNAMEPATTERN);
+        public void patternCheck(string pattern)
+        {
+            if (regName.IsMatch(pattern))
+            {
+                Console.WriteLine("Valid Pattern");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pattern");
+            }
+        }
+
+        // Main Method
         static void Main(string[] args)
         {
             Console.WriteLine("!!! Welcome to the User registration Problem.!!!");
+            Program p1 = new Program();
+            Console.WriteLine("Enter first name: ");
+            string firstName=Console.ReadLine();
+            p1.patternCheck(firstName);
         }
     }
 }
