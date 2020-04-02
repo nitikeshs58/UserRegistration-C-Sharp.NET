e37beaf536d24ec1dc9e90c78a6870ed7f3ccbe4
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-02 12:52:39 +0530

    Added: checked last name is valid or not.

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,9 +7,9 @@ namespace UserRegistration
     {
         static string VALIDNAMEPATTERN = "^[A-Z][a-z]{2,}$";
         Regex regName = new Regex(VALIDNAMEPATTERN);
-        public void patternCheck(string pattern)
+        public void firstNameCheck(string pattern1)
         {
-            if (regName.IsMatch(pattern))
+            if (regName.IsMatch(pattern1))
             {
                 Console.WriteLine("Valid Pattern");
             }
@@ -19,6 +19,19 @@ namespace UserRegistration
             }
         }
 
+        public void lastNameCheck(string pattern2)
+        {
+            if (regName.IsMatch(pattern2))
+            {
+                Console.WriteLine("Valid Pattern");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pattern");
+            }
+
+        }
+
         // Main Method
         static void Main(string[] args)
         {
@@ -26,7 +39,11 @@ namespace UserRegistration
             Program p1 = new Program();
             Console.WriteLine("Enter first name: ");
             string firstName=Console.ReadLine();
-            p1.patternCheck(firstName);
+            p1.firstNameCheck(firstName);
+
+            Console.WriteLine("Enter last name: ");
+            string lastName = Console.ReadLine();
+            p1.lastNameCheck(lastName);
         }
     }
 }
